02a079cfacadf3797d2929c20cec7af28c3b6065
BYPanfffff <114209578+BYPanfffff@users.noreply.github.com>
2022-12-01 19:07:48 +0800

    Add files via upload

---
+++ 6.37.c
@@ -0,0 +1,35 @@
+int recursiveMaximum(int data[], int n)
+{
+     if(n==0)
+    {
+        return data[0];
+    }
+    else
+    {
+        return 	data[n]>recursiveMaximum(data, n-1)? 
+				data[n]: recursiveMaximum(data, n-1);
+    }
+}
+
+
+
+
+ 
+int main(void)
+{
+	int i, x, n;
+	printf("°}¦C­Ó¼Æ:");
+	scanf("%d", &n);
+	printf("\n");
+////////////////////////////////////	
+	int arr[n];
+	printf("°}¦C¤¸¯À:");
+	for(i=0; i<n; i++){
+		scanf("%d", &arr[i]);
+	}
+	printf("\n");	
+////////////////////////////////////
+    int result = recursiveMaximum(arr, n-1);
+    printf("%d", result);
+    return 0;
+}
